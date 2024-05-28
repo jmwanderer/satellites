e85bcca46fbe7df7c524ac485fbebba3abf2c1d5
jim <jmwanderer@gmail.com>
2024-05-28 22:28:24 +0000

    Enable setting links up or down

--- mnet/link_event.sh
+++ mnet/link_event.sh
@@ -1,26 +1,44 @@
-curl http://127.0.0.1:8000/link \
-  -X PUT \
-  -H "Content-Type: application/json" \
-  -d '{
-    "node1_name": "R0_0",
-    "node2_name": "R1_0",
-    "up": false
-  }'
+if [ "$1" == "up" ];
+then
+  up="true"
+  echo "Set links up"
+else
+  up="false"
+  echo "Set links down"
+fi  
+
 
 curl http://127.0.0.1:8000/link \
   -X PUT \
   -H "Content-Type: application/json" \
-  -d '{
-    "node1_name": "R0_0",
-    "node2_name": "R0_1",
-    "up": false
-  }'
+  -d "{
+    \"node1_name\": \"R0_0\",
+    \"node2_name\": \"R1_0\",
+    \"up\": ${up}
+  }"
+
 curl http://127.0.0.1:8000/link \
   -X PUT \
   -H "Content-Type: application/json" \
-  -d '{
-    "node1_name": "R0_0",
-    "node2_name": "R2_2",
-    "up": false
-  }'
+  -d "{
+    \"node1_name\": \"R0_0\",
+    \"node2_name\": \"R0_1\",
+    \"up\": ${up}
+  }"
+curl http://127.0.0.1:8000/link \
+  -X PUT \
+  -H "Content-Type: application/json" \
+  -d "{
+    \"node1_name\": \"R0_0\",
+    \"node2_name\": \"R3_0\",
+    \"up\": ${up}
+  }"
+curl http://127.0.0.1:8000/link \
+  -X PUT \
+  -H "Content-Type: application/json" \
+  -d "{
+    \"node1_name\": \"R0_0\",
+    \"node2_name\": \"R0_3\",
+    \"up\": ${up}
+  }"
 
