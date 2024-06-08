3fe1e92e99ea816f7f42445d20c32e6f44f6b3dc
jim <jmwanderer@gmail.com>
2024-06-07 17:44:02 -0700

    Add clarifying comments

--- gps_sats.py
+++ gps_sats.py
@@ -7,18 +7,21 @@ from skyfield.api import load, wgs84
 import os
 
 def load_gps_sats():
+    # URL for GPS satellite information
     url = "https://celestrak.org/NORAD/elements/gp.php?GROUP=gps-ops&FORMAT=tle"
+
+    # Load TLE data into a local cache directory
+    # Note, will not reload after the first time until cache is cleared.
     if not os.path.exists("cache"):
         os.mkdir("cache")
-
-    # Note, will not reload after the first time until cache is cleared.
     satellites = load.tle_file(url, filename="cache/gps-ops.tle")
     print("Loadded %d satellites", len(satellites))
 
+    # Dump information for each satellite
+    ts = load.timescale()
     for sat in satellites:
         print(f"Name: {sat.name}")
         print("Epoch %s" % sat.epoch.utc_jpl())
-        ts = load.timescale()
         geo = sat.at(ts.now())
         lat, lon = wgs84.latlon_of(geo)
         height = wgs84.height_of(geo)
